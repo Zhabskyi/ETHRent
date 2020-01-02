dff513e172a01e475e1bd9fd54ff39cf02f6ebda
Zhabskyi <poltavagaz@gmail.com>
2020-01-02 16:14:06 +0000

    Initial commit. Add json file, src folder with index.js

---
+++ server/src/index.js
@@ -0,0 +1,10 @@
+const PORT = process.env.PORT || 8001;
+const ENV = require("./environment");
+
+const app = require("./application")(ENV);
+const server = require("http").Server(app);
+
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT} in ${ENV} mode.`);
+});
