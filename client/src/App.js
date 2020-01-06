3543f2224319503bbcdc9cde5351773e5450e3da
Zhabskyi <poltavagaz@gmail.com>
2020-01-06 10:35:24 -0700

    Replace SimpleStorage for Marketplace.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+//import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import MarketplaceContract from "./contracts/Marketplace.json";
 import getWeb3 from "./getWeb3";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
@@ -29,9 +30,9 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = MarketplaceContract.networks[networkId];
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        MarketplaceContract.abi,
         deployedNetwork && deployedNetwork.address
       );
 
