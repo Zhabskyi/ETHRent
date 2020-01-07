0d38180171ebef800d5b96761484012940086d41
Chris <chris.mastel@gmail.com>
2020-01-07 14:18:03 -0700

    add category radio buttons

--- client/src/components/items/Items.js
+++ client/src/components/items/Items.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 import classes from "./Items.module.scss";
 import ItemContext from "../../context/Item/ItemContext";
@@ -12,6 +12,7 @@ const Items = () => {
 
   const { items, getItems, loading } = itemContext;
   const { products } = blockchainContext;
+  const [checkedCategory, setCategory] = useState(false);
 
   useEffect(() => {
     getItems();
@@ -20,6 +21,34 @@ const Items = () => {
 
   return (
     <>
+      <form>
+        <div classname={classes.radio}>
+          <label>
+            <input type='radio' value='tools'
+              checked={checkedCategory === 'tools'}
+              onChange={(e) => {
+                setCategory(e.target.value);
+              }} />
+              Tools
+          </label>
+          <label>
+            <input type='radio' value='sports'
+              checked={checkedCategory === 'sports'}
+              onChange={(e) => {
+                setCategory(e.target.value);
+              }} />
+              Sports Equipment
+          </label>
+          <label>
+            <input type='radio' value='electronics'
+              checked={checkedCategory === 'electronics'}
+              onChange={(e) => {
+                setCategory(e.target.value);
+              }} />
+              Electronics
+          </label>
+        </div>
+      </form>
       {items !== null && !loading ? (
         <>
           {items.map(item => (
