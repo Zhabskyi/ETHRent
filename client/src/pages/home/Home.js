004193f525a33baa1f6052d2f1bf1fe57623c144
Chris <chris.mastel@gmail.com>
2020-01-08 12:39:08 -0700

    set up description block

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -13,8 +13,20 @@ const Home = () => {
     // eslint-disable-next-line
   }, []);
 
-  return (
+  return (    
     <>
+      <div className={classes.info}>
+        <h4 className={classes.banner}>Easy Peer-to-Peer Renting</h4>
+        <ul className={classes.benefits}>
+          <li>Find the gear you need for a short amount of time</li>
+          <li>Earn ether for your stuff while it's not in use</li>
+        </ul>
+        <ol className={classes.directions}>
+          <li>Transfer the required deposit to the Ethereum Blockchain</li>
+          <li>Use the item for as long as you want</li>
+          <li>Once the item is returned, the owner ends the Rental</li>
+        </ol>
+      </div>
       <div className={classes.manage}>
         {isAuthenticated ? <Link to='/my-items'>Manage my items</Link> : null}
       </div>
