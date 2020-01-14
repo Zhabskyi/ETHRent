51c3146a284b19fa6c3806296725c5eb04126212
Zhabskyi <poltavagaz@gmail.com>
2020-01-14 14:11:53 -0700

    Add test for get user request

--- server/src/db/schema/test.sql
+++ server/src/db/schema/test.sql
@@ -1,14 +1,13 @@
-INSERT INTO users (first_name, last_name, email, password, city, province, postal_code, phone_number, map)
-VALUES ('Alice', 'McLoud', 'mcloud@gmail.com', '123456', 'Calgary', 'Alberta', 'T3H5X5', '4034445566', "https://maps.googleapis.com/maps/api/staticmap?fillcolor:black&center=Lloydminster,SK,+SK+S9V+1Y1&zoom=16&size=400x400&key=AIzaSyBBLRkfiaPp3pIVgIiFhikE2yoIuyCKbwk");
-INSERT INTO users (first_name, last_name, email, password, city, province, postal_code, phone_number, map)
-VALUES ('John', 'Doe', 'doe@gmail.com', '123456', 'Calgary', 'Alberta', 'T3E6XS6', '4034445566', "https://maps.googleapis.com/maps/api/staticmap?fillcolor:black&center=Lloydminster,SK,+SK+S9V+1Y1&zoom=16&size=400x400&key=AIzaSyBBLRkfiaPp3pIVgIiFhikE2yoIuyCKbwk");
+INSERT INTO users (first_name, last_name, email, password, city, address, province, postal_code, phone_number, map)
+VALUES ('Alice', 'McLoud', 'mcloud@gmail.com', '123456', 'Calgary', 'street', 'AB', 'T3H5X5', '4034445566', 'https://maps.googleapis.com/maps');
+INSERT INTO users (first_name, last_name, email, password, city, address, province, postal_code, phone_number, map)
+VALUES ('John', 'Doe', 'doe@gmail.com', '123456', 'Calgary', 'street', 'AB', 'T3E6XS6', '4031111111', 'https://maps.googleapis.com/maps');
 
 
 INSERT INTO items (user_id,  created_on, title, description, daily_rate, deposit, photo)
 VALUES ('1', '2019-11-01', 'Hammer', 'Good for hitting nails', 2, 3, 'https://images.unsplash.com/photo-1566937169390-7be4c63b8a0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60');
 INSERT INTO items (user_id, created_on, title, description, daily_rate, deposit, photo)
 VALUES ('2', '2019-11-01', 'Hammer', 'Good for hitting nails', 3, 3, 'https://images.unsplash.com/photo-1566937169390-7be4c63b8a0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60');
-INSERT INTO items (user_id, created_on, title, description, daily_rate, deposit, photo)
-VALUES ('3', '2019-11-01', 'Hammer', 'Good for hitting nails', 3, 3, 'https://images.unsplash.com/photo-1566937169390-7be4c63b8a0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60');
+
 
 
---
+++ server/src/routes/__tests__/users.test.js
@@ -0,0 +1,51 @@
+const request = require("supertest");
+const generateApplication = require("../../application");
+
+describe("Users", () => {
+  let app;
+
+  beforeAll(() => {
+    app = generateApplication("test");
+  });
+
+  afterEach(async () => {
+    await request(app).post("/api/debug/reset");
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  test("GET /api/users", async () => {
+    await request(app)
+      .get("/api/users")
+      .expect(200, [
+        {
+          id: 1,
+          first_name: "Alice",
+          last_name: "McLoud",
+          email: "mcloud@gmail.com",
+          password: "123456",
+          city: "Calgary",
+          address: "street",
+          province: "AB",
+          postal_code: "T3H5X5",
+          phone_number: "4034445566",
+          map: "https://maps.googleapis.com/maps"
+        },
+        {
+          id: 2,
+          first_name: "John",
+          last_name: "Doe",
+          email: "doe@gmail.com",
+          password: "123456",
+          city: "Calgary",
+          address: "street",
+          province: "AB",
+          postal_code: "T3E6XS6",
+          phone_number: "4031111111",
+          map: "https://maps.googleapis.com/maps"
+        }
+      ]);
+  });
+});
