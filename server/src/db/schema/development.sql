f42075fc5e7996525758e31bdffc46bdcce816be
Zhabskyi <poltavagaz@gmail.com>
2020-01-14 11:06:01 -0700

    Install Jest on server and refactor seeds files

--- server/src/db/schema/development.sql
+++ server/src/db/schema/development.sql
@@ -12,10 +12,3 @@ VALUES ('2', '2019-11-01', 'Hammer', 'Good for hitting nails', 3, 3, 'https://im
 INSERT INTO items (user_id, created_on, title, description, daily_rate, deposit, photo)
 VALUES ('3', '2019-11-01', 'Hammer', 'Good for hitting nails', 3, 3, 'https://images.unsplash.com/photo-1566937169390-7be4c63b8a0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60');
 
-INSERT INTO transactions (item_id, owner_id,  borrower_id, start_date)
-VALUES ('1', '1', '2','2019-11-03');
-INSERT INTO transactions (item_id, owner_id,  borrower_id, start_date)
-VALUES ('2', '1', '3', '2019-11-03');
-INSERT INTO transactions (item_id, owner_id,  borrower_id, start_date)
-VALUES ('3', '2', '1', '2019-11-03');
-
